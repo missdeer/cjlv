839deebdb1e814e4cb9a3c15dd36853cf41b4c54
Fan Yang <missdeer@dfordsoft.com>
2016-06-13 16:09:58 +0800

    (*)open logic

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 
 #include <QApplication>
 #include <QActionGroup>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -21,22 +24,55 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionOpenZipLogBundle_triggered()
 {
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Open Cisco Jabber Log Zip Bundle"),
+                                                    QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+                                                    tr("Cisco Jabber Log Zip Bundle (*.zip);;All files (*.*)"));
+    if (fileName.isEmpty())
+        return;
 
+    QMessageBox::information(this, "Opened zip file", fileName, QMessageBox::Ok);
 }
 
 void MainWindow::on_actionOpenRawLogFile_triggered()
 {
+    QStringList fileNames = QFileDialog::getOpenFileNames(this,
+                                                          tr("Open Cisco Jabber Log Files"),
+                                                          QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+                                                          tr("Cisco Jabber Log Files (jabber.log*);;All files (*.*)"));
 
+    if (fileNames.isEmpty())
+        return;
+
+    QMessageBox::information(this, "Opened Raw Log Files", fileNames.join(", "), QMessageBox::Ok);
 }
 
 void MainWindow::on_actionOpenLogFolder_triggered()
 {
+    QString dir = QFileDialog::getExistingDirectory(this,
+                                                    tr("Open Cisco Jabber Log Folder"),
+                                                    QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+                                                    QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
 
+    if (dir.isEmpty())
+        return;
+
+    QMessageBox::information(this, "Opened Cisco Jabber Log Folder", dir, QMessageBox::Ok);
 }
 
 void MainWindow::on_actionOpenCurrentInstalledJabberLogFolder_triggered()
 {
+#if defined(Q_OS_WIN)
+    QString dir = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "\\AppData\\Local\\Cisco\\Unified Communications\\Jabber\\CSF\\Logs";
+#endif
+#if defined(Q_OS_MAC)
+    QString dir = QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Library/Log/Jabber";
+#endif
 
+    if (dir.isEmpty())
+        return;
+
+    QMessageBox::information(this, "Opened Current Installed Cisco Jabber Log Folder", dir, QMessageBox::Ok);
 }
 
 void MainWindow::on_actionClose_triggered()
