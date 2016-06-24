65ccbd13e06d05cd1575dea06f0e890a0dde93dc
Fan Yang <missdeer@dfordsoft.com>
2016-06-24 11:05:44 +0800

    (*)code clean

--- main.cpp
+++ main.cpp
@@ -11,32 +11,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    const char *compilation_date = __DATE__;
-    const char *months[] = {"Jan","Feb","Mar","Apr","May","Jun",
-                        "Jul","Aug","Sep","Oct","Nov","Dec",NULL};
-    int     i;
-    QDate rc;
-
-    for( i = 0; months[i] != NULL; i++ )
-        if( memcmp( compilation_date, months[i], 3 ) == 0 )
-            break;
-
-    if( months[i] == NULL )
-        rc = QDate( 1900,01,01 );
-    else
-    {
-        char year[5], day[3];
-
-        memcpy(year,compilation_date+7,4);
-        year[4] = 0x00;
-        memcpy(day,compilation_date+4,2);
-        day[2] = 0x00;
-
-        rc = QDate(atoi(year),i+1,atoi(day));
-    }
-
-    qDebug() << __DATE__ <<rc << QDate::currentDate() << rc.daysTo(QDate::currentDate());
-    if (rc.daysTo(QDate::currentDate()) > 365)
+    QDate d =  QLocale(QLocale::C).toDate(QString(__DATE__).simplified(), QLatin1String("MMM d yyyy"));
+    if (d.daysTo(QDate::currentDate()) > 365)
     {
         QMessageBox::critical(NULL, "Expired", "This application has been expired, please contact fyang3@cisco.com for a new build.", QMessageBox::Ok );
         return 1;
