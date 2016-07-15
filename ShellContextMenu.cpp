9a701fa958c86db8b38b2c5581157909b172b987
Fan Yang <missdeer@dfordsoft.com>
2016-07-15 10:55:07 +0800

    (*)append custom context menu items

--- ShellContextMenu.cpp
+++ ShellContextMenu.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <QtCore>
+#include <QList>
 #include <Shobjidl.h>
 #include <Shlobj.h>
 #include "ShellContextMenu.h"
@@ -52,18 +53,37 @@ void CShellContextMenu::ShowContextMenu(QMenu *menu, QWidget* parent, QPoint &pt
 	if (!hMenu)
 		return ;
 	if (SUCCEEDED(imenu->QueryContextMenu(hMenu, 0, MIN_ID, MAX_ID, CMF_NORMAL)))
-	{
+	{		
+		QList<QAction *> actions = menu->actions();
+		UINT_PTR id = MAX_ID + 1;
+		::AppendMenuW(hMenu, MF_SEPARATOR, id++, 0);
+		Q_FOREACH(QAction* action, actions)
+		{
+			if (action->text().isEmpty())
+				::AppendMenuW(hMenu, MF_SEPARATOR, id++, 0);
+			else
+				::AppendMenuW(hMenu, MF_STRING, id++, action->text().toStdWString().c_str());
+		}
 		int iCmd = ::TrackPopupMenuEx(hMenu, TPM_RETURNCMD, pt.x(), pt.y(), (HWND)parent->winId(), NULL);
 		if (iCmd > 0)
 		{
-			CMINVOKECOMMANDINFOEX info = { 0 };
-			info.cbSize = sizeof(info);
-			info.fMask = CMIC_MASK_UNICODE;
-			info.hwnd = (HWND)parent->winId();
-			info.lpVerb  = MAKEINTRESOURCEA(iCmd - 1);
-			info.lpVerbW = MAKEINTRESOURCEW(iCmd - 1);
-			info.nShow = SW_SHOWNORMAL;
-			imenu->InvokeCommand((LPCMINVOKECOMMANDINFO)&info);
+			if (iCmd > MAX_ID + 1)
+			{
+				QAction* action = actions.at(iCmd - MAX_ID - 1 -1);
+				if (action)
+					emit action->triggered();
+			}
+			else
+			{
+				CMINVOKECOMMANDINFOEX info = { 0 };
+				info.cbSize = sizeof(info);
+				info.fMask = CMIC_MASK_UNICODE;
+				info.hwnd = (HWND)parent->winId();
+				info.lpVerb  = MAKEINTRESOURCEA(iCmd - 1);
+				info.lpVerbW = MAKEINTRESOURCEW(iCmd - 1);
+				info.nShow = SW_SHOWNORMAL;
+				imenu->InvokeCommand((LPCMINVOKECOMMANDINFO)&info);				
+			}
 		}
 	}
 	DestroyMenu(hMenu);
