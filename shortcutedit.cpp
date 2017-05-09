93a832a4bfef7fe027e4c4522c2fe29b56ca3c49
Fan Yang <me@minidump.info>
2017-05-09 11:10:11 +0800

    (*)shortcut edit WIP

--- shortcutedit.cpp
+++ shortcutedit.cpp
@@ -6,3 +6,46 @@ ShortcutEdit::ShortcutEdit(QWidget *parent)
 {
 
 }
+
+bool ShortcutEdit::event(QEvent *e)
+{
+    if (e->type() == QEvent::KeyPress){
+        QKeyEvent *keyEvent = static_cast<QKeyEvent*>(e);
+
+        int keyInt = keyEvent->key();
+        Qt::Key key = static_cast<Qt::Key>(keyInt);
+        if(key == Qt::Key_unknown){
+            qDebug() << "Unknown key from a macro probably";
+            return false;
+        }
+        // the user have clicked just and only the special keys Ctrl, Shift, Alt, Meta.
+        if(key == Qt::Key_Control ||
+                key == Qt::Key_Shift ||
+                key == Qt::Key_Alt ||
+                key == Qt::Key_Meta)
+        {
+            qDebug() << "Single click of special key: Ctrl, Shift, Alt or Meta";
+            setText( QKeySequence(keyInt).toString(QKeySequence::NativeText));
+            return true;
+        }
+
+        // check for a combination of user clicks
+        Qt::KeyboardModifiers modifiers = keyEvent->modifiers();
+        QString keyText = keyEvent->text();
+        // if the keyText is empty than it's a special key like F1, F5, ...
+        qDebug() << "Pressed Key:" << keyText;
+
+        QList<Qt::Key> modifiersList;
+        if(modifiers & Qt::ShiftModifier)
+            keyInt += Qt::SHIFT;
+        if(modifiers & Qt::ControlModifier)
+            keyInt += Qt::CTRL;
+        if(modifiers & Qt::AltModifier)
+            keyInt += Qt::ALT;
+        if(modifiers & Qt::MetaModifier)
+            keyInt += Qt::META;
+
+        setText( QKeySequence(keyInt).toString(QKeySequence::NativeText));
+    }
+    return QLineEdit::event(e);
+}
--- shortcutedit.h
+++ shortcutedit.h
@@ -3,10 +3,15 @@
 
 #include <QLineEdit>
 
+QT_BEGIN_NAMESPACE
+class QEvent;
+QT_END_NAMESPACE
+
 class ShortcutEdit : public QLineEdit
 {
 public:
     explicit ShortcutEdit(QWidget* parent = 0);
+    bool event(QEvent *e);
 };
 
 #endif // SHORTCUTEDIT_H
